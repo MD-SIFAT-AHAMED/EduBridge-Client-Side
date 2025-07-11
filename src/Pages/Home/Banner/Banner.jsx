4c279e905d4c1172867d2e3bd967da33c7d5aa5c
MD SIFAT AHAMED~ <mdsifat978@gmail.com>
2025-07-11 20:33:22 +0600

    Navbar and banner added

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,11 +1,55 @@
-import React from 'react';
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; //
+import banner1 from "../../../assets/BannerImg/banner1.jpg";
+import banner3 from "../../../assets/BannerImg/banner3.jpg";
 
 const Banner = () => {
-    return (
-        <div>
-            
+  return (
+    <div>
+      <Carousel
+        autoPlay={true}
+        showThumbs={false}
+        infiniteLoop={true}
+        showStatus={false}
+        showArrows={false}
+      >
+        <div className="relative w-full ">
+          <img
+            src={banner1}
+            alt="Banner"
+            className="w-full h-auto object-cover"
+          />
+
+          <div className="absolute inset-0 flex flex-col lg:items-start justify-center bg-black/40 text-white p-10">
+            <h3 className="text-lg md:text-4xl font-bold mb-2">
+              Unlock Your Potential <br /> Learn Anytime, Anywhere
+            </h3>
+            <p className="text-sm md:text-lg text-gray-300">
+              Start your journey to success with expert-led courses designed
+              just for you.
+            </p>
+          </div>
         </div>
-    );
+        <div className="relative w-full">
+          <img
+            src={banner3}
+            alt="Banner"
+            className="w-full h-auto object-cover"
+          />
+
+          <div className="absolute inset-0 flex flex-col lg:items-start justify-center bg-black/40 text-white p-10">
+            <h3 className="text-lg md:text-4xl font-bold mb-2">
+              Share Your Knowledge
+              <br /> Inspire the World
+            </h3>
+            <p className="text-sm md:text-lg text-gray-300">
+              Turn your expertise into income by teaching what you love.
+            </p>
+          </div>
+        </div>
+      </Carousel>
+    </div>
+  );
 };
 
 export default Banner;
--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,11 +1,17 @@
 import { createBrowserRouter } from "react-router";
 import MainLayout from "../Main/MainLayout";
+import Home from "../Pages/Home/Home/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
     Component: MainLayout,
-    children: [],
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+    ],
   },
 ]);
 
