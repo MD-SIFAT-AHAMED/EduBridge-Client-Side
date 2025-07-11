9da645fcfb16679e68d6e8bf30e48191cefac644
MD SIFAT AHAMED~ <mdsifat978@gmail.com>
2025-07-12 02:49:57 +0600

    firebase initial setup complete

---
+++ src/Context/AuthContext/AuthProvider.jsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from "react";
+import { AuthContex } from "./AuthContext";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
+
+import { auth } from "../../Firebase/Firebase.init";
+const provider = new GoogleAuthProvider();
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const updateUserData = (userData) => {
+    return updateProfile(auth.currentUser, userData);
+  };
+
+  const loginWithGoggle = () => {
+    return signInWithPopup(auth, provider);
+  };
+
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      console.log("OnSate user info", currentUser);
+      setLoading(false);
+    });
+    return () => unSubscribe();
+  }, []);
+
+  const authInfo = {
+    user,
+    logOut,
+    signIn,
+    loading,
+    loginWithGoggle,
+    updateUserData,
+    createUser,
+  };
+  return <AuthContex value={authInfo}>{children}</AuthContex>;
+};
+
+export default AuthProvider;
